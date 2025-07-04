43a5f0010fd9b8943b5ea2629f48e4ced4b02ec6
jwheo22 <heoj18908@gmail.com>
2025-07-05 00:33:15 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,39 +1,35 @@
-num = 0
+def Game():
+    num = 0
+    player = 'A'
 
-player = 'A'
+    while True:
+        while True:
+            try:
+                n = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+                if n < 1 or n > 3:
+                    raise Exception('1, 2, 3 중 하나를 입력하세요')
+            except ValueError:
+                print('정수를 입력하세요')
+                continue
+            except Exception as e:
+                print(e)
+            else:
+                break
+        if player == 'A':
+            for _ in range(n):
+                num = num + 1
+                if num == 31:
+                    return('A')
+                print(f'playerA: {num}')
+            player = 'B'
+            continue
 
-while (True):
-    try:
-        n = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
-        if n < 1 or n > 3:
-            raise Exception('1, 2, 3 중 하나를 입력하세요')
-    except ValueError:
-        print('정수를 입력하세요')
-        continue
-    except Exception as e:
-        print(e)
-    else:
-        break
+        if player == 'B':
+            for _ in range(n):
+                num = num + 1
+                if num == 31:
+                    return('B')
+                print(f'playerB: {num}')
+            player = 'A'
 
-for _ in range(n):
-    num = num + 1
-    print(f'playerA: {num}')
-
-player = 'B'
-
-while (True):
-    try:
-        n = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
-        if n < 1 or n > 3:
-            raise Exception('1, 2, 3 중 하나를 입력하세요')
-    except ValueError:
-        print('정수를 입력하세요')
-        continue
-    except Exception as e:
-        print(e)
-    else:
-        break
-
-for _ in range(n):
-    num = num + 1
-    print(f'playerB: {num}')
+Game()
