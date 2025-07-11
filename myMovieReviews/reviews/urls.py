ace086b4316312bafa9fbf9106ce73440a531128
jwheo22 <heoj18908@gmail.com>
2025-07-12 04:22:00 +0900

    필수 구현 완료

--- myMovieReviews/reviews/urls.py
+++ myMovieReviews/reviews/urls.py
@@ -4,5 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('', reviews_list),
-    path('<int:pk>/', reviews_detail)
+    path('<int:pk>/', reviews_detail),
+    path('create/', reviews_create),
+    path('<int:pk>/delete/', reviews_delete),
+    path('<int:pk>/update/', reviews_update),
 ]
--- myMovieReviews/reviews/views.py
+++ myMovieReviews/reviews/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Review
 
 # Create your views here.
@@ -11,3 +11,43 @@ def reviews_detail(request, pk):
     review = Review.objects.get(id=pk)
     context = {"review": review}
     return render(request, 'reviews_detail.html', context)
+
+def reviews_create(request):
+    if request.method == 'POST':
+        Review.objects.create(
+            title = request.POST['title'],
+            year = request.POST['year'],
+            genre = request.POST['genre'],
+            rating = request.POST['rating'],
+            running_time = request.POST['running_time'],
+            content = request.POST['content'],
+            director = request.POST['director'],
+            actors = request.POST['actors'],
+        )
+        return redirect('/reviews/')
+    return render(request, 'reviews_create.html')
+
+def reviews_delete(request, pk):
+    if request.method == 'POST':
+        review = Review.objects.get(id=pk)
+        review.delete()
+    return redirect('/reviews/')
+
+def reviews_update(request, pk):
+    review = Review.objects.get(id=pk)
+
+
+    if request.method == 'POST':
+        review.title = request.POST['title']
+        review.year = request.POST['year']
+        review.genre = request.POST['genre']
+        review.rating = request.POST['rating']
+        review.running_time = request.POST['running_time']
+        review.content = request.POST['content']
+        review.director = request.POST['director']
+        review.actors = request.POST['actors']
+        review.save()
+        return redirect(f"/reviews/{pk}/")
+        
+    context = {'review': review}
+    return render(request, 'reviews_update.html', context)
