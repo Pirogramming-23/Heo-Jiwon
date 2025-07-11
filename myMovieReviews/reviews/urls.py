0f88eba3caf9b99490570cf84024c90263883cf2
jwheo22 <heoj18908@gmail.com>
2025-07-12 02:20:19 +0900

    기능 2,4,5 구현

--- myMovieReviews/reviews/urls.py
+++ myMovieReviews/reviews/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', reviews_list)
+    path('', reviews_list),
+    path('<int:pk>/', reviews_detail)
 ]
--- myMovieReviews/reviews/views.py
+++ myMovieReviews/reviews/views.py
@@ -6,3 +6,8 @@ def reviews_list(request):
     reviews = Review.objects.all()
     context = {"reviews": reviews}
     return render(request, 'reviews_list.html', context)
+
+def reviews_detail(request, pk):
+    review = Review.objects.get(id=pk)
+    context = {"review": review}
+    return render(request, 'reviews_detail.html', context)
