97155c95f2881f836263997c28b7fe4530dd3939
jwheo22 <heoj18908@gmail.com>
2025-07-14 13:15:25 +0900

    과제 오류 수정

--- myMovieReviews/reviews/models.py
+++ myMovieReviews/reviews/models.py
@@ -5,7 +5,7 @@ class Review(models.Model):
     title = models.CharField(max_length=32)
     year = models.IntegerField()
     genre = models.CharField(max_length=32)
-    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
     running_time = models.PositiveIntegerField()
     content = models.TextField()
     director = models.CharField(max_length=32)
